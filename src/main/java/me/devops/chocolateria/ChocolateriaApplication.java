f18ab505c52598ce0ecfa85242c51cf5459ed307
Matías Varela <matias.varela@scanntech.com>
2025-09-23 19:24:18 -0300

    first commit

---
+++ src/main/java/me/devops/chocolateria/ChocolateriaApplication.java
@@ -0,0 +1,13 @@
+package me.devops.chocolateria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChocolateriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChocolateriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/devops/chocolateria/ChocolateriaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.devops.chocolateria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChocolateriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
